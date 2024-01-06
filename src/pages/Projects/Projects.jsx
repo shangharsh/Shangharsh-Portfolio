1b13616840a1664fb4a576fc0af95602b5ea844a
Shangharsh Adhikari <san.adh1996@gmail.com>
2024-01-06 22:32:39 +0545

    Feat: Showed Projects inside the Cards

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,8 +1,65 @@
 import React from 'react'
+import { Button, Card } from 'react-bootstrap'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className='d-flex flex-row flex-wrap gap-3 justify-content-center align-items-center pt-3 pb-3'>
+      <Card style={{width: '18rem'}}>
+        <Card.Header className='text-center'>
+          Quiz Hub
+        </Card.Header>
+        <Card.Body className='pt-3 pb-3 ps-3 pe-3'>
+          <Card.Img variant='top' src='/Quiz.png' alt='project_image'/>
+        </Card.Body>
+        <Card.Footer className='text-center'>
+          <Button>View Details</Button>
+        </Card.Footer>
+      </Card>
+      <Card style={{width: '18rem'}}>
+        <Card.Header className='text-center'>
+          Quiz Hub
+        </Card.Header>
+        <Card.Body className='pt-3 pb-3 ps-3 pe-3'>
+          <Card.Img variant='top' src='/Quiz.png' alt='project_image'/>
+        </Card.Body>
+        <Card.Footer className='text-center'>
+          <Button>View Details</Button>
+        </Card.Footer>
+      </Card>
+      <Card style={{width: '18rem'}}>
+        <Card.Header className='text-center'>
+          Quiz Hub
+        </Card.Header>
+        <Card.Body className='pt-3 pb-3 ps-3 pe-3'>
+          <Card.Img variant='top' src='/Quiz.png' alt='project_image'/>
+        </Card.Body>
+        <Card.Footer className='text-center'>
+          <Button>View Details</Button>
+        </Card.Footer>
+      </Card>
+      <Card style={{width: '18rem'}}>
+        <Card.Header className='text-center'>
+          Quiz Hub
+        </Card.Header>
+        <Card.Body className='pt-3 pb-3 ps-3 pe-3'>
+          <Card.Img variant='top' src='/Quiz.png' alt='project_image'/>
+        </Card.Body>
+        <Card.Footer className='text-center'>
+          <Button>View Details</Button>
+        </Card.Footer>
+      </Card>
+      <Card style={{width: '18rem'}}>
+        <Card.Header className='text-center'>
+          Quiz Hub
+        </Card.Header>
+        <Card.Body className='pt-3 pb-3 ps-3 pe-3'>
+          <Card.Img variant='top' src='/Quiz.png' alt='project_image'/>
+        </Card.Body>
+        <Card.Footer className='text-center'>
+          <Button>View Details</Button>
+        </Card.Footer>
+      </Card>
+    </div>
   )
 }
 
